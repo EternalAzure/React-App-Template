6d1e7e599bc183ffdebdb894959bd83d972b9047
EternalAzure <kall.miika@gmail.com>
2021-09-07 22:52:57 +0300

    install-commands muutettiin ajettavaksi batch tiedostoksi

---
+++ backend/install-commands.bat
@@ -0,0 +1,7 @@
+call npm install jsonwebtoken
+call npm install bcrypt
+call npm install mongoose
+call npm install mongoose-unique-validator
+call npm install express
+call npm install dotenv
+call npm --save-dev eslint
